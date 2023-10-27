94594de47d9fcb4e264872c4a76abd7479242252
nordengt <nordenghisingt123@gmail.com>
2023-10-27 16:40:29 +0545

    no classname plot

--- ldm/utils.py
+++ ldm/utils.py
@@ -5,14 +5,17 @@ from torch.utils.data import DataLoader
 def accuracy_fn(pred, y):
     return (pred.argmax(dim=1) == y).sum().item() / len(pred)
 
-def show_images(dataset, class_names, rows=3, cols=3):
+def show_images(dataset, class_names=None, rows=3, cols=3):
     fig = plt.figure(figsize=(9, 9))
     for i in range(1, rows * cols + 1):
         idx = torch.randint(0, len(dataset), size=[1]).item()
-        image, label = dataset[idx]
         fig.add_subplot(rows, cols, i)
+        if class_names:
+            image, label = dataset[idx]
+            plt.title(class_names[label])
+        else:
+            image = dataset[idx]
         plt.imshow(image.squeeze(), cmap="gray")
-        plt.title(class_names[label])
         plt.axis(False)
 
 def dataloader(train_dataset, test_dataset, batch_size):
