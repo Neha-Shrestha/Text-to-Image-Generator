6c083c0a18e4b905435528e0e2f3e2efeac965bb
nordengt <nordenghisingt123@gmail.com>
2023-12-27 20:24:01 +0545

    update

--- ldm/time_embedding.py
+++ ldm/time_embedding.py
@@ -4,24 +4,21 @@ import torch.nn.functional as F
 import math
 
 class TimestepEmbedding(nn.Module):
-    def __init__(self, n_channels):
+    def __init__(self, n_channels, n_embedding):
+        super().__init__()
         self.n_channels = n_channels
-        self.n_embedding = self.n_channels * 4
+        self.n_embedding = n_embedding
         self.timestep_mlp = nn.Sequential(
+            nn.BatchNorm1d(self.n_channels),
+            nn.SiLU(),
             nn.Linear(self.n_channels, self.n_embedding),
             nn.SiLU(),
             nn.Linear(self.n_embedding, self.n_embedding)
         )
 
     def forward(self, t):
-        emb = -math.log(10000) * torch.linspace(0, 1, self.n_embedding//2)
-        emb = t[:, None] * emb.exp()[None, :]
-        emb = torch.cat((emb.sin(), emb.cos()), dim=1)
+        exponent = -math.log(10000) * torch.linspace(0, 1, self.n_channels//2, device=t.device)
+        emb = t[:, None].float() * exponent.exp()[None, :]
+        emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
         emb = self.timestep_mlp(emb)
         return emb
-
-# def timestep_embedding(tsteps, emb_dim, max_period= 10000):
-#     exponent = -math.log(max_period) * torch.linspace(0, 1, emb_dim//2, device=tsteps.device)
-#     emb = tsteps[:,None].float() * exponent.exp()[None,:]
-#     emb = torch.cat([emb.sin(), emb.cos()], dim=-1)
-#     return F.pad(emb, (0,1,0,0)) if emb_dim%2==1 else emb
