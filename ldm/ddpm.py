b9130e468ff2a7e8f58f5bcaf50bf77748001499
nordengt <nordenghisingt123@gmail.com>
2023-12-29 20:59:53 +0545

    update ddpm

--- ldm/ddpm.py
+++ ldm/ddpm.py
@@ -1,3 +1,6 @@
+import torch
+from utils import init_attr
+
 class DDPM:
     def __init__(self, beta_min, beta_max, n_steps):
         init_attr(self, locals=locals())
@@ -6,13 +9,13 @@ class DDPM:
         self.alpha_bar = self.alpha.cumprod(dim=0)
         self.sigma = self.beta.sqrt()
     
-    def forward(self, x0):
-        device = x0.device
+    def schedule(self, x0):
+        device = "cuda"
         n = len(x0)
-        t = torch.randint(0, self.n_steps, (n,), device=device, dtype=torch.long)
-        noise = torch.randn(x0.shape, device=device)
+        t = torch.randint(0, self.n_steps, (n,), dtype=torch.long)
+        noise = torch.randn(x0.shape)
         alpha_bar_t = self.alpha_bar[t].reshape(-1, 1, 1, 1).to(device)
         mean = alpha_bar_t.sqrt().to(device) * x0.to(device) 
         variance = (1-alpha_bar_t).sqrt().to(device) * noise.to(device) 
         xt = mean + variance
-        return (xt, t.to(device)), noise
+        return (xt, t.to(device)), noise.to(device)
