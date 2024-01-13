78a9ba5903e3284f9b7857246c95df08f06e1f75
nordengt <nordenghisingt123@gmail.com>
2024-01-13 18:28:49 +0545

    fix ddpm

--- ldm/ddpm.py
+++ ldm/ddpm.py
@@ -33,12 +33,12 @@ class DDPM:
             t = torch.full((batch_size,), i, dtype=torch.long, device=device)
             noise_pred = model(xt, t, c)
             noise = (torch.randn_like(xt) if i > 0 else torch.zeros_like(xt)).to(device)
-            alpha_t1 = self.alpha[t - 1] if t > 0 else torch.tensor(1)
-            beta_bar_t = 1 - self.alpha[t]
+            alpha_t1 = self.alpha[i - 1] if i > 0 else torch.tensor(1)
+            beta_bar_t = 1 - self.alpha[i]
             beta_bar_t1 = 1 - alpha_t1
             x_0_hat = ((x_t - beta_bar_t.sqrt() * noise_pred) / self.alpha[t].sqrt()).clamp(-1, 1)
-            x0_coeff = alpha_t1.sqrt() * (1 - self.alpha[t]) / beta_bar_t
+            x0_coeff = alpha_t1.sqrt() * (1 - self.alpha[i]) / beta_bar_t
             xt_coeff = self.alpha[t].sqrt() * beta_bar_t1 / beta_bar_t
-            x_t = x_0_hat * x0_coeff + x_t * xt_coeff + self.sigma[t] * z
+            x_t = x_0_hat * x0_coeff + x_t * xt_coeff + self.sigma[i] * noise
             preds.append(x_t.cpu())
         return preds
