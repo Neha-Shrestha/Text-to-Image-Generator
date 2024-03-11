34831bf20a4b4430ef3db6c95b5ce4cc55ec772f
nordengt <nordenghisingt123@gmail.com>
2024-03-11 14:51:03 +0545

    fix

--- ldm/scratch/unet.py
+++ ldm/scratch/unet.py
@@ -2,7 +2,19 @@ import torch
 from torch import nn
 
 from time_embedding import *
-from attention import *
+
+class SelfAttention(nn.Module):
+    def __init__(self, ni, attn_chans):
+        super().__init__()
+        self.attn = nn.MultiheadAttention(ni, ni//attn_chans, batch_first=True)
+        self.norm = nn.BatchNorm2d(ni)
+
+    def forward(self, x):
+        n, c, h, w = x.shape
+        x_norm = self.norm(x.clone()) 
+        x_norm = x_norm.view(n, c, -1).transpose(1, 2)
+        x = self.attn(x_norm, x_norm, x_norm, need_weights=False)[0]
+        return x.transpose(1,2).reshape(n,c,h,w)
 
 def unet_conv(in_channels, out_channels, kernel_size=3, stride=1, act=nn.SiLU, norm=None, bias=True):
     layers = nn.Sequential()
