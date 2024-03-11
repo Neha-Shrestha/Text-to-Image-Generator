ced2ac9a704be285201c246fa0ac17c9412ccf79
nordengt <nordenghisingt123@gmail.com>
2024-03-11 15:57:08 +0545

    timeembedding inside unet

--- ldm/scratch/unet.py
+++ ldm/scratch/unet.py
@@ -1,7 +1,28 @@
 import torch
+import torch.nn.functional as F
+import math
+
 from torch import nn
 
-from time_embedding import *
+class TimestepEmbedding(nn.Module):
+    def __init__(self, n_channels, n_embedding):
+        super().__init__()
+        self.n_channels = n_channels
+        self.n_embedding = n_embedding
+        self.timestep_mlp = nn.Sequential(
+            nn.BatchNorm1d(self.n_channels),
+            nn.SiLU(),
+            nn.Linear(self.n_channels, self.n_embedding),
+            nn.SiLU(),
+            nn.Linear(self.n_embedding, self.n_embedding)
+        )
+
+    def forward(self, t):
+        exponent = -math.log(10000) * torch.linspace(0, 1, self.n_channels//2, device=t.device)
+        emb = t[:, None].float() * exponent.exp()[None, :]
+        emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
+        emb = self.timestep_mlp(emb)
+        return emb
 
 class SelfAttention(nn.Module):
     def __init__(self, ni, attn_chans):
