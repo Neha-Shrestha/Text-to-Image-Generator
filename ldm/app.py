7665a0a964a5daab61b4e8dbfddd6ad6ef9a14f8
nordengt <nordenghisingt123@gmail.com>
2024-03-14 00:01:31 +0545

    nearly final

--- ldm/app.py
+++ ldm/app.py
@@ -1,28 +1,10 @@
 import gradio as gr
 from ldm import generate_image
-from trainer import train
+from trainer import train, cancel
+from testing import compute_fid
 
 with gr.Blocks(title="Text-to-Image Generator") as app:
     gr.Markdown("# Text-to-Image Generator: 7th Semester Project")
-    with gr.Tab("Training"):
-        gr.Markdown("### Train a Text-to-Image Generator by inserting following Hyperparameters.")
-        with gr.Row():
-            with gr.Column(scale=5):
-                epochs = gr.Textbox(label="Epochs")
-                lr = gr.Textbox(label="Learning Rate")
-                loss_fn = gr.Radio(["MSELoss", "MAELoss"], label="Loss Function")
-                optimizer = gr.Radio(["Adam", "SGD"], label="Optimizer")
-                train_btn = gr.Button("Train")
-                
-            with gr.Column(scale=5):
-                training_loss = gr.Label(label="Training Loss Value")
-                training_image = gr.Image(label="Training Generated Image")
-                train_btn.click(
-                    train, 
-                    inputs=[epochs, lr, loss_fn, optimizer], 
-                    outputs=[training_loss, training_image]
-                )
-        
     with gr.Tab("Inference"):
         gr.Markdown("### Insert a text and generate a face.")
         with gr.Row():
@@ -38,35 +20,47 @@ with gr.Blocks(title="Text-to-Image Generator") as app:
             
             with gr.Column():
                 image_output = gr.Image(label="Generated image", width=500)
+                
+            with gr.Column():
+                gr.Image(label="Model Loss", value="./images/final.png", width=500)
+            
+        with gr.Row():
+            with gr.Column():
+                inference_output = gr.Image(label="Timesteps")
                 generate_btn.click(
                     generate_image,
                     inputs=[text], 
-                    outputs=[image_output],
-                )      
+                    outputs=[image_output, inference_output],
+                )
+
+    with gr.Tab("Training"):
+        gr.Markdown("### Train a Text-to-Image Generator by inserting following Hyperparameters.")
+        with gr.Row():
+            epochs = gr.Textbox(label="Epochs", value=1000)
+            batch_size = gr.Textbox(label="Batch Size", value=20)
+            lr = gr.Textbox(label="Learning Rate", value=0.001)
         
-            with gr.Column():
-                gr.Image(label="Model Loss", value="./images/final.png", width=500)
+        with gr.Row():
+            loss_fn = gr.Radio(["MSE", "MAE"], label="Loss Function", value="MSE")
+            optimizer = gr.Radio(["Adam", "SGD"], label="Optimizer", value="Adam")
+            latent_folder = gr.Textbox(label="Latent Folder Location", value="./data/face/images")
+
+        with gr.Row():
+            preprocess = gr.Radio(["Yes", "No"], label="Preprocessing", value="No")
+            data_folder = gr.Textbox(label="Data Folder Location")
+        
+        with gr.Row():
+            train_btn = gr.Button("Train")
+            cancel_btn = gr.Button("Cancel")
+        
+        progress_output = gr.Textbox()
+        
+        train_btn.click(
+            train, 
+            inputs=[epochs, batch_size, lr, loss_fn, optimizer, latent_folder, preprocess, data_folder],
+            outputs=progress_output
+        )
+        cancel_btn.click(cancel)
 
 if __name__ == "__main__":
     app.launch()
-
-# import gradio as gr
-# import ldm
-
-# iface = gr.Interface(
-#     fn=ldm.generate_image,
-#     inputs=gr.Textbox(lines=1, label="Enter text here"),
-#     outputs="image",
-#     title="Text-to-Image Generator",
-#     description="Provide name and generate face.",
-#     theme="default",
-#     allow_flagging=False,
-#     examples=[
-#         ["Barack Obama"],
-#         ["Cristiano Ronaldo"],
-#         ["Donald Trump"]
-#     ]
-# )
-
-# if __name__ == "__main__":
-#     iface.launch()
--- ldm/inference.py
+++ ldm/inference.py
@@ -1,5 +1,5 @@
 import torch
-from utils import get_class_tensor, latents_to_pil, save_images
+from utils import get_class_tensor, latents_to_pil, save_images, plot_images_from_folder
 
 def inference(unet, ddpm_scheduler, vae, device, num_images=1, text=None, img_name=None):
     noisy_images = torch.randn((num_images, 4, 32, 32)).to(device)
@@ -13,12 +13,17 @@ def inference(unet, ddpm_scheduler, vae, device, num_images=1, text=None, img_na
 
     for timestep in ddpm_scheduler.timesteps:
         unet.eval()
+        img_checkpoints = [999, 700, 500, 200, 150, 120, 100, 80, 60, 40, 20, 0]
         with torch.inference_mode():
             noise = unet(noisy_images, timestep, labels).sample
         noisy_images = ddpm_scheduler.step(noise, timestep, noisy_images).prev_sample
+        if timestep.item() in img_checkpoints and text:
+            latents_to_pil(vae, noisy_images, save_path=f"./images/inference/{timestep}.png")
 
     images = latents_to_pil(vae, noisy_images)
     
-    if text: return images[0]
+    if text: 
+        inference_img = plot_images_from_folder("./images/inference")
+        return images[0], inference_img
     
     save_images(img_name, images, labels)
